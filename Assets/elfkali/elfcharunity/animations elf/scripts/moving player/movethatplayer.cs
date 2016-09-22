b4cebf5a4efe4f8b9bddeb41d1ab9e1294e3d6e9
kanestapler <kanestapler@gmail.com>
2016-09-22 09:52:19 -0500

    adding in elf, animating it, adding small stuff.

---
+++ Assets/elfkali/elfcharunity/animations elf/scripts/moving player/movethatplayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movethatplayer : MonoBehaviour {
+    static Animator ani;
+    // Use this for initialization
+    void Start () {
+        ani = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Jump")) {
+            GrabCrown();
+        } else if (vMove > 0.2) {
+            print("Moving");
+            ani.SetBool("isJogging", true);
+        } else {
+            ani.SetBool("isJogging", false);
+        }
+
+    }
+
+    void GrabCrown() {
+        ani.SetTrigger("isjumping");
+    }
+}
