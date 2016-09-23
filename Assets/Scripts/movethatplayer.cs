07f84a32aa28f07f16d03a5fb188c897a338b196
kanestapler <kanestapler@gmail.com>
2016-09-23 12:05:58 -0500

    Making player move and attack better

--- Assets/Scripts/movethatplayer.cs
+++ Assets/Scripts/movethatplayer.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class movethatplayer : MonoBehaviour {
 
+    private readonly string JOGGING = "isJogging";
+    private readonly string JUMPING = "isJumping";
+    private readonly string ATTACK = "attack";
+
     public float speed;
     public float turnSpeed;
     public float axisThreshold;
+    public int fireWaitTime;
 
     private static Animator ani;
     private float hMove;
     private float vMove;
     private float angH;
     private float angV;
+    private int fireTime;
 
     void Start () {
         ani = GetComponent<Animator>();
+        fireTime = 0;
     }
 	
 	// Update is called once per frame
@@ -23,27 +30,48 @@ public class movethatplayer : MonoBehaviour {
         vMove = Input.GetAxis("Vertical");
         angH = Input.GetAxis("RightH");
         angV = Input.GetAxis("RightV");
+
+        Move();
+
+        if (Input.GetButtonDown("Fire1")) {
+            Fire();
+        }
+        if (!ReadyToFire()) {
+            fireTime++;
+        }
         if (Input.GetButtonDown("Jump")) {
             GrabCrown();
-        } else if (vMove > 0) { //Moving forward
-            ani.SetBool("isJogging", true);
-        } else { //Not moving forward
-            ani.SetBool("isJogging", false);
-        }
-        Move();
-        if (Input.GetButton("Fire1")) {
-            ani.SetTrigger("attack");
         }
     }
 
     private void GrabCrown() {
-        ani.SetTrigger("isjumping");
+        ani.SetBool(JOGGING, false);
+        ani.SetTrigger(JUMPING);
     }
 
     private void Move() {
-        transform.Translate(hMove * speed * Time.deltaTime, 0.0f,vMove * speed * Time.deltaTime);
+        if (vMove > 0) { //Moving forward
+            ani.SetBool(JOGGING, true);
+            transform.Translate(hMove * speed * Time.deltaTime, 0.0f, vMove * speed * Time.deltaTime);
+        } else if (vMove < 0) { //Moving backwards
+            transform.Translate(hMove * speed * Time.deltaTime, 0.0f, vMove * speed * Time.deltaTime);
+        } else {
+            ani.SetBool(JOGGING, false);
+        }
         if (Mathf.Abs(angV) > axisThreshold || Mathf.Abs(angH) > axisThreshold) {
             transform.Rotate(0, angH * turnSpeed * Time.deltaTime, 0);
         }
+    }
+
+    private void Fire() {
+        if (ReadyToFire()) {
+            fireTime = 1;
+            ani.SetTrigger(ATTACK);
+        }
+    }
+
+
+    private bool ReadyToFire() {
+        return fireTime % fireWaitTime == 0;
     }
 }
