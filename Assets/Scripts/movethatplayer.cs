53fc4584bb94ef62ee0f6531f71567dc65a34566
kanestapler <kanestapler@gmail.com>
2016-09-22 20:16:41 -0500

    Making player rotate! Had to change input manager though

--- Assets/Scripts/movethatplayer.cs
+++ Assets/Scripts/movethatplayer.cs
@@ -9,8 +9,8 @@ public class movethatplayer : MonoBehaviour {
     private static Animator ani;
     private float hMove;
     private float vMove;
-    //private float angH;
-    //private float angV;
+    private float angH;
+    private float angV;
 
     void Start () {
         ani = GetComponent<Animator>();
@@ -20,8 +20,8 @@ public class movethatplayer : MonoBehaviour {
 	void Update () {
         hMove = Input.GetAxis("Horizontal");
         vMove = Input.GetAxis("Vertical");
-        //angH = Input.GetAxis("RightH");
-        //angV = Input.GetAxis("RightV");
+        angH = Input.GetAxis("RightH") * 60;
+        angV = Input.GetAxis("RightV") * 45;
         if (Input.GetButtonDown("Jump")) {
             GrabCrown();
         } else if (vMove > 0) { //Moving forward
@@ -38,7 +38,6 @@ public class movethatplayer : MonoBehaviour {
 
     private void Move() {
         transform.Translate(hMove * speed * Time.deltaTime, 0.0f,vMove * speed * Time.deltaTime);
-        //Should rotate using the right joy stick
-        //transform.localEulerAngles = new Vector3(angV*turnSpeed, angH*turnSpeed, 0);
+        transform.Rotate(angV * turnSpeed * Time.deltaTime, angH * turnSpeed * Time.deltaTime, 0);
     }
 }
