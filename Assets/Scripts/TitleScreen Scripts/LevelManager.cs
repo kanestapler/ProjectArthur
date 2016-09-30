3dbec6b2433e286a282a44ff4b0635acbd77abac
Ashley N <Ashley N>
2016-09-29 20:35:52 -0500

    Added Title Screen, Splash Screen is in Process

---
+++ Assets/Scripts/TitleScreen Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void LoadMenu(string name)
+    {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(name) ;
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
