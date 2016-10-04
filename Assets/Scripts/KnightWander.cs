588225dbe0e02813e7046aeb70f1946e0149584a
kanestapler <kanestapler@gmail.com>
2016-10-04 10:45:00 -0500

    Starting moving npc

---
+++ Assets/Scripts/KnightWander.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnightWander : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float distanceFromWall = 1;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        FrontHit();
+    }
+
+    private bool FrontHit () {
+        RaycastHit RCHit;
+        Vector3 direction = new Vector3(0,0,0);
+        print(Physics.Raycast(transform.position, direction, out RCHit, distanceFromWall));
+        return true;
+    }
+
+    private bool LeftHit() {
+        return true;
+    }
+
+    private bool RightHit() {
+        return true;
+    }
+
+    private bool BackHit() { //Maybe not needed?
+        return true;
+    }
+
+}
