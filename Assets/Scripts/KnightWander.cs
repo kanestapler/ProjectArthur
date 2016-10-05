d6a3d989613bb3d45ee300e86892843c85cf8a54
kanestapler <kanestapler@gmail.com>
2016-10-04 21:57:58 -0500

    Making the knight pace between 2 spots

--- Assets/Scripts/KnightWander.cs
+++ Assets/Scripts/KnightWander.cs
@@ -5,17 +5,42 @@ public class KnightWander : MonoBehaviour {
 
     public float speed = 1.0f;
     public float distanceFromWall = 10;
+    public Vector3 destination1;
+    public Vector3 destination2;
+    public float distanceThreshhold = 3.0f;
 
     private NavMeshAgent NMAgent;
+    private int currentDestination;
 
     void Start() {
         NMAgent = GetComponent<NavMeshAgent>();
         NMAgent.destination = new Vector3(0.0f,0.0f,0.0f);
+        currentDestination = 1;
     }
 
     void Update() {
-        print(FrontHit());
-        //transform.Translate(0.0f,0.0f,0.0f);
+        if (ShouldITurnAround()) {
+            if (currentDestination == 1) {
+                NMAgent.destination = destination2;
+                currentDestination = 2;
+            } else {
+                NMAgent.destination = destination1;
+                currentDestination = 1;
+            }
+        }
+    }
+
+    private bool ShouldITurnAround() {
+        if (currentDestination == 1) {
+            if (Vector3.Distance(transform.position, destination1) <= distanceThreshhold) {
+                return true;
+            }
+        } else {
+            if (Vector3.Distance(transform.position, destination2) <= distanceThreshhold) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private bool FrontHit () {
@@ -26,18 +51,6 @@ public class KnightWander : MonoBehaviour {
             print(RCHit.distance);
         }
         return Physics.Raycast(transform.position, direction, out RCHit, distanceFromWall);
-    }
-
-    private bool LeftHit() {
-        return true;
-    }
-
-    private bool RightHit() {
-        return true;
-    }
-
-    private bool BackHit() { //Maybe not needed?
-        return true;
     }
 
 }
