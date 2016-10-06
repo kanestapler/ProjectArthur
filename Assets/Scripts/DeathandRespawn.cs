f38fe1484872143a643b399b320c1495b99a5670
Ben <bhoskins@stu.jsu.edu>
2016-10-05 21:02:07 -0500

    Rough Respawn

---
+++ Assets/Scripts/DeathandRespawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathandRespawn : MonoBehaviour {
+
+    public GameObject Spawner;
+    private GameObject clone;
+
+    void OnTriggerEnter(Collider other)
+    {
+        //if we have a death animation, we can put a delay here.
+        Debug.Log("collision");
+        if (other.CompareTag("Knight"))//or whoever can kill this.gameobject
+        {
+            Debug.Log("Knight attacked");
+            clone = this.gameObject;
+            Instantiate(clone, Spawner.transform.position, Spawner.transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
