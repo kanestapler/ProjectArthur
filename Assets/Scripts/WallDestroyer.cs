7967866d7fd643990eba9afca5532285ddadf77c
kanestapler <kanestapler@gmail.com>
2016-10-05 23:07:19 -0500

    Making maze destroy in center for throne

---
+++ Assets/Scripts/WallDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallDestroyer : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Wall") {
+            print("BOOM");
+            Destroy(other.gameObject);
+        }
+    }
+}
