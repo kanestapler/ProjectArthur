d24027ca7d90c28b90a329e0cb4b60ec71c34eef
Ben <bhoskins@stu.jsu.edu>
2016-10-07 10:51:33 -0500

    Respawn roughly works

--- Assets/Scripts/DeathandRespawn.cs
+++ Assets/Scripts/DeathandRespawn.cs
@@ -4,18 +4,12 @@ using System.Collections;
 public class DeathandRespawn : MonoBehaviour {
 
     public GameObject Spawner;
-    private GameObject clone;
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        //if we have a death animation, we can put a delay here.
-        Debug.Log("collision");
-        if (other.CompareTag("Knight"))//or whoever can kill this.gameobject
+        if (other.gameObject.CompareTag("Knight"))
         {
-            Debug.Log("Knight attacked");
-            clone = this.gameObject;
-            Instantiate(clone, Spawner.transform.position, Spawner.transform.rotation);
-            Destroy(this.gameObject);
+            this.transform.position = Spawner.transform.position;
         }
     }
 
