4fca67d696ad76fa8183c584b89ca4e354c3311a
Ashley N <Ashley N>
2016-10-07 22:38:43 -0500

    More Menu Options Added

--- Assets/Scripts/TitleScreen Scripts/LevelManager.cs
+++ Assets/Scripts/TitleScreen Scripts/LevelManager.cs
@@ -11,6 +11,16 @@ public class LevelManager : MonoBehaviour {
 		UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
     }
 
+	public void HowToPlay(string name)
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("HowToPlay");
+	}
+
+	public void ExitBacktoMenu (string name)
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("TitleScreen");
+	}
+
 	public void QuitGame()
 	{
 		Application.Quit ();
