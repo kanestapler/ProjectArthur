9c54f4cc5988773256b34ef5710d7437fcab940a
kanestapler <kanestapler@gmail.com>
2016-10-09 14:13:03 -0500

    Making players kill each other

---
+++ Assets/Scripts/Dagger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dagger : MonoBehaviour {
+
+    private int playerNumber;
+
+	// Use this for initialization
+	void Start () {
+        if (gameObject.tag == "Dagger1") {
+            playerNumber = 1;
+        } else if (gameObject.tag == "Dagger2") {
+            playerNumber = 2;
+        } else {
+            print("ERROR: DAGGER NEEDS CORRECT TAG");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            if (other.gameObject.GetComponent<PlayerController>().playerNumber != playerNumber) {
+                other.gameObject.GetComponent<DeathandRespawn>().RespawnMe();
+            }
+        }
+    }
+}
